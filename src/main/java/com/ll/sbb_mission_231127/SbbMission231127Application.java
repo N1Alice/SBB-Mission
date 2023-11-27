22bc4f5156a2300380ca8644f35c962c81918a73
N1Alice <kc700370@gmail.com>
2023-11-27 09:52:51 +0900

    setting

---
+++ src/main/java/com/ll/sbb_mission_231127/SbbMission231127Application.java
@@ -0,0 +1,13 @@
+package com.ll.sbb_mission_231127;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbbMission231127Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbbMission231127Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/sbb_mission_231127/SbbMission231127ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.sbb_mission_231127;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbbMission231127ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
