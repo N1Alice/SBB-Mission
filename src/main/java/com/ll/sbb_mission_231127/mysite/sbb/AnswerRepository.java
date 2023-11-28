e2e02752b523bf2154e569d28afd3c4d10cad11d
N1Alice <kc700370@gmail.com>
2023-11-29 01:57:08 +0900

    sbb 2-05 Repository

---
+++ src/main/java/com/ll/sbb_mission_231127/mysite/sbb/AnswerRepository.java
@@ -0,0 +1,6 @@
+package com.ll.sbb_mission_231127.mysite.sbb;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnswerRepository extends JpaRepository<Answer, Integer> {
+}
